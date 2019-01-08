829d74797b5cdb1711dc3b3ec9e3e9ce180536f5
Alison Reilly <reillyamr@gmail.com>
2019-01-08 21:35:25 +0000

    Automatically backed up by Learn

---
+++ lib/music_library_controller.rb
@@ -0,0 +1,19 @@
+class MusicLibraryController 
+  attr_accessor :path 
+  
+  def initialize(path = nil)
+    
+    if path != nil 
+      
+    end 
+    
+  end 
+  
+  def call 
+  end
+  
+  
+  
+  
+  
+end 
